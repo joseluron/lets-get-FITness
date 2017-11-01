a7ee6611402505fb4b476e11831ec7e716d88013
Jose Luis C C <joseluron@gmail.com>
2017-11-01 20:36:58 +0100

    Added custom validators to User model. Improved errors processing in users/createUser route

--- models/user.js
+++ models/user.js
@@ -2,11 +2,114 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+let usernameLengthChecker = (username) => {
+    if(!username) {
+        return false;
+    } else {
+        if(username.length < 3 || username.length > 18) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
+  
+let validUsername = (username) => {
+    if(!username) {
+        return false;
+    } else {
+        const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+        return regExp.test(username);
+    }
+};
+  
+ const usernameValidators = [
+    {
+        validator: usernameLengthChecker,
+        message: 'El nombre de usuario debe tener un mínimo de 3 caracteres pero un máximo de 18'
+    },
+    {
+        validator: validUsername,
+        message: 'El nombre de usuario no puede contener caracteres especiales'
+    }
+];
+
+let emailLengthChecker = (email) => {
+    if(!email) {
+        return false;
+    } else {
+        if(email.length < 5 || email.length > 30) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
+  
+let validEmailChecker = (email) => {
+    if(!email) {
+        return false;
+    } else {
+        const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        return regExp.test(email);
+    }
+};
+  
+ const emailValidators = [
+    {
+        validator: emailLengthChecker,
+        message: 'El email debe tener un mínimo de 5 caracteres pero un máximo de 30'
+    },
+    {
+        validator: validEmailChecker,
+        message: 'El email debe tener un formato apropiado'
+    }
+];
+  
+ let passwordLengthChecker = (password) => {
+    if(!password) {
+        return false;
+    } else {
+        if(password.length <4 || password.length > 20) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
+  
+ const passwordValidators = [
+    {
+        validator: passwordLengthChecker,
+        message: 'El password debe tener un mínimo de 4 caracteres pero un máximo de 15'
+    }
+];
 
 const userSchema = new Schema({
-    username: { type: String, required: true, unique: true, lowercase: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true }
+    username: { type: String, required: true, unique: true, lowercase: true, validate: usernameValidators },
+    email: { type: String, required: true, unique: true, lowercase: true, validate: emailValidators },
+    password: { type: String, required: true, validate: passwordValidators }
 });
+
+userSchema.pre('save', function(next) {
+    if(!this.isModified('password')) {
+      return next();
+    }
+  
+    bcrypt.hash(this.password, null, null, (err, hash) => {
+        if(err) {
+            return next(err);
+        } else {
+            this.password = hash;
+            next();
+        }
+    });
+});
+  
+userSchema.methods.comparePassword = (password) => {
+    return bcrypt.compareSync(password, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,61 @@ const User = require('../models/user');
 module.exports = (router) => {
 
     router.post('/createUser', (req, res) => {
-        res.send('Test');
+        if(!req.body.username) {
+            res.json({ success: false, message: 'Debe introducir un nombre de usuario' });
+        } else {
+            if(!req.body.email) {
+                res.json({ success: false, message: 'Debe introducir un correo electrónico' });
+            } else {
+                if(!req.body.password) {
+                    res.json({ success: false, message: 'Debe introducir una contraseña' });
+                } else {
+                    let user = new User({
+                        username: req.body.username.toLowerCase(),
+                        email: req.body.email.toLowerCase(),
+                        password: req.body.password
+                    });
+                    user.save((err) => {
+                        if(err) {
+                            if(err.code === 11000){
+                                User.findOne({ username: req.body.username }, (err, user) => {
+                                    if(err) {
+                                        res.json({ success: false, message: err });
+                                    } else {
+                                        if(user) {
+                                            res.json({ success: false, message: 'Nombre de usuario ya registrado' });
+                                        } else {
+                                            res.json({ success: false, message: 'Correo electrónico ya registrado' });
+                                        }
+                                    }
+                                });
+                            } else {
+                                if(err.errors) {
+                                    if(err.errors.username) {
+                                        res.json({ success: false, message: err.errors.username.message });
+                                    } else {
+                                        if(err.errors.email) {
+                                            res.json({ success: false, message: err.errors.email.message});
+                                        } else {
+                                            if(err.errors.password) {
+                                                res.json({ success: false, message: err.errors.password.message });
+                                            } else {
+                                                res.json({ success: false, message: err });
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    console.log(err);
+                                    res.json({ success: false, message: 'No se ha podido crear el usuario. Error: ', err });
+                                }
+                            }
+                        } else {
+                            res.json({ success: true, message: 'Usuario creado correctamente' });
+                        }
+                    });
+                }
+            }
+        }
     });
     
     return router;
