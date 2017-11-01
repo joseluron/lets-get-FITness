3ca8aeb63092a2cf48c68906251d35280a90cc17
Jose Luis C C <joseluron@gmail.com>
2017-11-01 16:11:51 +0100

    Created navbar.component. Added navbar to app.component header

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     HomeComponent
   ],
   imports: [
