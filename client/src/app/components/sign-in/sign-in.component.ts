4b772080fe51e3719f404d96c39e7cc968628959
Jose Luis C C <joseluron@gmail.com>
2017-11-08 20:44:37 +0100

    The sign in page will now redirect to the last url after loging in

--- client/src/app/components/sign-in/sign-in.component.ts
+++ client/src/app/components/sign-in/sign-in.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 
 import { AuthenticationService } from '../../services/authentication.service';
 
+import { AuthenticatedGuard } from '../../guards/authenticated.guard';
+
 import { SignInUser } from '../../models/signInUser';
 
 @Component({
@@ -17,10 +19,12 @@ export class SignInComponent implements OnInit {
   message: string;
   messageClass: string;
   processing = false;
+  previousUri: string;
 
   constructor(
     private formBuilder: FormBuilder,
     private authenticationService: AuthenticationService,
+    private authenticatedGuard: AuthenticatedGuard,
     private router: Router
   ) {
     this.createSignInForm();
@@ -67,13 +71,23 @@ onSignIn() {
       this.message = data.message;
       this.authenticationService.storeUserData(data.token, data.user);
       setTimeout(() => {
-        this.router.navigate(['/all-routines']);
+        if (this.previousUri) {
+          this.router.navigate([this.previousUri]);
+        } else {
+          this.router.navigate(['/all-routines']);
+        }
       }, 2000);
     }
   });
 }
 
   ngOnInit() {
+    if (this.authenticatedGuard.redirectUri) {
+      this.messageClass = 'alert alert-danger';
+      this.message = 'Debe haber iniciado sesión para poder ver esa página';
+      this.previousUri = this.authenticatedGuard.redirectUri;
+      this.authenticatedGuard.redirectUri = undefined;
+    }
   }
 
 }
--- client/src/app/guards/authenticated.guard.ts
+++ client/src/app/guards/authenticated.guard.ts
@@ -5,15 +5,21 @@ import { AuthenticationService } from '../services/authentication.service';
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
 
+    redirectUri: string;
+
     constructor(
         private authenticationService: AuthenticationService,
         private router: Router
     ) { }
 
-    canActivate() {
+    canActivate(
+        activatedRouteSnapshot: ActivatedRouteSnapshot,
+        routerStateSnapshot: RouterStateSnapshot
+    ) {
         if (this.authenticationService.userSignedIn()) {
             return true;
         } else {
+            this.redirectUri = routerStateSnapshot.url;
             this.router.navigate(['/sign-in']);
             return false;
         }
