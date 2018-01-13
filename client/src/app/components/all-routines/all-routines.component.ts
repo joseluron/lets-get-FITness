9f646e8c681a773fd45bcc74179fc6dccb118d4c
Jose Luis C C <joseluron@gmail.com>
2018-01-13 20:49:05 +0100

    Created new routine blueprint in all-routines.html

--- client/src/app/components/all-routines/all-routines.component.ts
+++ client/src/app/components/all-routines/all-routines.component.ts
@@ -7,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllRoutinesComponent implements OnInit {
 
+  message: string;
+  messageClass: string;
+  newRoutine = false;
+  reloadingRoutines = false;
+
   constructor() { }
+
+  newRoutineForm() {
+    this.newRoutine = true;
+  }
+
+  reloadAllRoutines() {
+    this.reloadingRoutines = true;
+    // Get all routines
+    setTimeout(() => {
+      this.reloadingRoutines = false;
+    }, 4000);
+  }
+
+  draftComment() {
+
+  }
 
   ngOnInit() {
   }
