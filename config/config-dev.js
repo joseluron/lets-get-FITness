d8bf6a42ec4f16a80cd9239747e5845838fddf12
Jose Luis C C <joseluron@gmail.com>
2018-04-11 22:22:11 +0200

    Last fixes

--- config/config-dev.js
+++ config/config-dev.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
-    uri: 'mongodb://localhost:27017/lets-get-FITness-dev',
+    uri: 'mongodb://localhost:27017/lets-get-FITness',
     secret: crypto,
     db: 'lets-get-FITness'
 }
